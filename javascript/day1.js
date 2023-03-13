a2cc587b2cbc382acdc06a0493fe328f96d298c4
talarys <d.simon.ssx0@gmail.com>
2023-03-13 23:31:22 +0100

    Refactor

--- javascript/day1.js
+++ javascript/day1.js
@@ -1,33 +1,24 @@
 import fs from 'fs';
 
-const input = fs
+const lines = fs
   .readFileSync('../inputs/day1.txt')
   .toString('utf-8')
   .split('\n');
 
-const parse = (lines) => {
-  const sums = [];
-  let acc = 0;
+let n = 1;
+let Elves = [0];
 
-  for (let line of lines) {
-    if (line) {
-      acc += Number(line);
-    } else {
-      sums.push(acc);
-      acc = 0;
-    }
+for (let line of lines) {
+  if (line) {
+    Elves[n - 1] += Number(line);
+  } else {
+    n += 1;
+    Elves[n - 1] = 0;
   }
+}
 
-  if (acc) {
-    sums.push(acc);
-  }
-
-  return sums;
-};
-
-const part1 = Math.max(...parse(input));
-const part2 = parse(input)
-  .sort()
+const part1 = Math.max(...Elves);
+const part2 = Elves.sort()
   .slice(-3)
   .reduce((a, b) => a + b);
 
