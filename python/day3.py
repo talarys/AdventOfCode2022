842dc8be38a5ca41a5bda3b9525624996c8e08ae
talarys <d.simon.ssx0@gmail.com>
2023-03-13 20:24:37 +0100

    Finished day3 python

---
+++ python/day3.py
@@ -0,0 +1,27 @@
+lines = open("../inputs/day3.txt").read().strip().split("\n")
+
+from more_itertools import grouper
+
+
+def score(c):
+    if c.islower():
+        return ord(c) - ord("a") + 1
+    return ord(c) - ord("A") + 1 + 26
+
+
+part1 = 0
+
+for line in lines:
+    mid = len(line) // 2
+    char = set(line[:mid]) & set(line[mid:])
+    part1 += score(char.pop())
+
+part2 = 0
+
+for group in grouper(lines, 3):
+    part2 += score(set.intersection(*map(set, group)).pop())
+
+
+if __name__ == "__main__":
+    print(part1)
+    print(part2)
