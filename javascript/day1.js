42b6d6a0cf99fcb68e24de610b57cf1a6cb4283f
talarys <d.simon.ssx0@gmail.com>
2023-10-15 22:23:58 +0200

    Made it more readable

--- javascript/day1.js
+++ javascript/day1.js
@@ -3,25 +3,18 @@ import fs from 'fs'
 const lines = fs
   .readFileSync('../inputs/day1.txt')
   .toString('utf-8')
-  .split('\n')
+  .split('\n\n')
 
-let n = 0
-let Elves = [0]
+const getSumOfGroup = (group) =>
+  group
+    .split('\n')
+    .map(Number)
+    .reduce((sum, num) => sum + num, 0)
 
-for (let line of lines) {
-  if (line) {
-    Elves[n] += Number(line)
-  } else {
-    n += 1
-    Elves[n] = 0
-  }
-}
+const sortedGroupSums = lines.map(getSumOfGroup).sort((a, b) => b - a)
 
-const part1 = Math.max(...Elves)
-
-const part2 = Elves.sort()
-  .slice(-3)
-  .reduce((a, b) => a + b)
+const part1 = sortedGroupSums[0]
+const part2 = sortedGroupSums.slice(0, 3).reduce((a, b) => a + b, 0)
 
 console.log(part1)
 console.log(part2)
